ba0d3622e62d40f8fba8de63444b742f842bc7c7
Johannes Brachem <jobrachem@posteo.de>
2019-02-25 16:14:35 +0100

    encode mongodb user

--- src/alfred/settings.py
+++ src/alfred/settings.py
@@ -177,7 +177,7 @@ class ExperimentSpecificSettings(object):
         self.mongo_saving_agent.host = config_parser.get('mongo_saving_agent', 'host')
         self.mongo_saving_agent.database = config_parser.get('mongo_saving_agent', 'database')
         self.mongo_saving_agent.collection = config_parser.get('mongo_saving_agent', 'collection')
-        self.mongo_saving_agent.user = config_parser.get('mongo_saving_agent', 'user')
+        self.mongo_saving_agent.user = base64.b64decode(config_parser.get('mongo_saving_agent', 'user')).decode("utf-8")
         self.mongo_saving_agent.password = base64.b64decode(config_parser.get('mongo_saving_agent', 'password')).decode("utf-8")
 
         self.fallback_local_saving_agent = _DictObj()
@@ -201,7 +201,7 @@ class ExperimentSpecificSettings(object):
         self.fallback_mongo_saving_agent.host = config_parser.get('fallback_mongo_saving_agent', 'host')
         self.fallback_mongo_saving_agent.database = config_parser.get('fallback_mongo_saving_agent', 'database')
         self.fallback_mongo_saving_agent.collection = config_parser.get('fallback_mongo_saving_agent', 'collection')
-        self.fallback_mongo_saving_agent.user = config_parser.get('fallback_mongo_saving_agent', 'user')
+        self.fallback_mongo_saving_agent.user = base64.b64decode(config_parser.get('fallback_mongo_saving_agent', 'user')).decode("utf-8")
         self.fallback_mongo_saving_agent.password = base64.b64decode(config_parser.get('fallback_mongo_saving_agent', 'password')).decode("utf-8")
 
         self.level2_fallback_local_saving_agent = _DictObj()
