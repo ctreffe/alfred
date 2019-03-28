d53d1fb1f90469526a48cde89033455ca8ac40ef
Johannes Brachem <jobrachem@posteo.de>
2019-03-28 13:40:53 +0100

    change default settings to work with mortimer v2

--- src/alfred/settings.py
+++ src/alfred/settings.py
@@ -204,8 +204,8 @@ class ExperimentSpecificSettings(object):
         self.fallback_mongo_saving_agent.host = config_parser.get('fallback_mongo_saving_agent', 'host')
         self.fallback_mongo_saving_agent.database = config_parser.get('fallback_mongo_saving_agent', 'database')
         self.fallback_mongo_saving_agent.collection = config_parser.get('fallback_mongo_saving_agent', 'collection')
-        self.fallback_mongo_saving_agent.user = base64.b64decode(config_parser.get('fallback_mongo_saving_agent', 'user')).decode("utf-8")
-        self.fallback_mongo_saving_agent.password = base64.b64decode(config_parser.get('fallback_mongo_saving_agent', 'password')).decode("utf-8")
+        self.fallback_mongo_saving_agent.user = config_parser.get('fallback_mongo_saving_agent', 'user')
+        self.fallback_mongo_saving_agent.password = config_parser.get('fallback_mongo_saving_agent', 'password')
 
         self.level2_fallback_local_saving_agent = _DictObj()
         self.level2_fallback_local_saving_agent.use = config_parser.getboolean('level2_fallback_local_saving_agent', 'use')
