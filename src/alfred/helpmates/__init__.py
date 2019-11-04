6c543892ac6e118f82a65cfda815e101e3520cf5
Johannes Brachem <jobrachem@posteo.de>
2019-11-04 18:11:43 +0100

    hotfix unicode debian server error

--- src/alfred/helpmates/__init__.py
+++ src/alfred/helpmates/__init__.py
@@ -71,7 +71,7 @@ def parse_xml_to_dict(path, interface='web', code=False):
             raise RuntimeError('input must be unicode or dict')
         return input
 
-    data_in = open(path, 'r').read()
+    data_in = open(path, 'r').read().decode('utf-8')
     data_in.replace('\r\n', '\n')
     data_out = xmltodict.parse(data_in)
     if interface == 'web' and not code:
