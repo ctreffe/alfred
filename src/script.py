995280dcf38e2ae82042a70abd0ba39b8e4b3d33
Johannes Brachem <jobrachem@posteo.de>
2020-03-09 17:21:23 +0100

    Update script.py

--- src/script.py
+++ src/script.py
@@ -4,44 +4,42 @@
 # - Section 1: Module imports - #
 #################################
 
-from alfred.page import *
-from alfred.section import *
-from alfred.element import *
-from alfred.layout import *
-from alfred.helpmates import *
+import alfred.section as sec
+import alfred.element as elm
 from alfred import Experiment
+from alfred.page import Page
+from alfred.helpmates import parse_xml_to_dict
+
 
 #################################################
 # - Section 2: Global variables and functions - #
 #################################################
 
 #################################
-# - Section 3: Custom classes - #
+# - Section 3: Page definitions - #
 #################################
 
 class Welcome(Page):
 
     def on_showing(self):
-        el = TextElement(self.values.test)
+        el = elm.TextElement(self.values.welcome_text)
         self.append(el)
 
 ########################################
 # - Section 4: Experiment generation - #
 ########################################
 
-
 def generate_experiment(self, custom_settings=None):
     exp = Experiment(custom_settings=custom_settings)
 
     # --- Page 1 --- #
-    page01 = Welcome(title="Hello, world!", values={"test": "test"})
+    page01 = Welcome(title="Hello, world!", values={"welcome_text": "test"})
 
     # Sections
-    main = SegmentedSection()
+    main = sec.SegmentedSection()
     main.append_items(page01)
 
     # Initialize and fill experiment
-    
     exp.append(main)
 
     return exp
