660c167253672c7d8a9bb46b1d0dda7cf91a9cff
Johannes Brachem <jobrachem@posteo.de>
2020-04-16 20:12:07 +0200

    Update page.py

--- src/alfred/page.py
+++ src/alfred/page.py
@@ -129,8 +129,8 @@ class PageCore(ContentCore):
         elif self._run_on_showing == 'always':
             self.on_hiding_widget()
             self.on_hiding()
-            self._log.append(('debug',
-                             'The current page executes the "on_hiding" method every time the page is hidden. If you want to turn this behavior off, please use "run_on_hiding=\'once\'" in the page initialisation.'))
+            # self._log.append(('debug',
+            #                  'The current page executes the "on_hiding" method every time the page is hidden. If you want to turn this behavior off, please use "run_on_hiding=\'once\'" in the page initialisation.'))
 
         self._has_been_hidden = True
         self.print_log()
