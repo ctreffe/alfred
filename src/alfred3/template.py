c0a98add05c57c4282a0abb5754a59ea13677a16
Johannes Brachem <jobrachem@posteo.de>
2020-05-27 10:06:59 +0200

    improve security

--- src/alfred3/template.py
+++ src/alfred3/template.py
@@ -14,6 +14,8 @@ import os
 import shutil
 import sys
 from pathlib import Path
+from werkzeug.utils import secure_filename
+from uuid import uuid4
 
 
 def donwload_template(url: str, target_dir: str = None):
@@ -39,17 +41,20 @@ def donwload_template(url: str, target_dir: str = None):
         with zipfile.ZipFile(file_name, "r") as zip_ref:
             zip_ref.extractall(target_dir)
     else:
+        dirname = secure_filename(target_dir)
+        if not dirname:
+            dirname = uuid4().hex
+            print("There was a problem with the directory name. A random name was used instead.")
         with zipfile.ZipFile(file_name, "r") as zip_ref:
-            zip_ref.extractall(target_dir)
-
+            zip_ref.extractall(dirname)
             namelist = zip_ref.namelist()
 
         # move files to target dir and remove zip directory
         directory_name = namelist[0]
-        subdir = os.path.join(target_dir, directory_name)
+        subdir = os.path.join(dirname, directory_name)
 
         for element in os.listdir(subdir):
-            shutil.move(os.path.join(subdir, element), target_dir)
+            shutil.move(os.path.join(subdir, element), dirname)
 
         os.rmdir(subdir)
 
@@ -67,11 +72,12 @@ if __name__ == "__main__":
 
     elif len(sys.argv) == 2:
         url = hello_world
-        donwload_template(url, sys.argv[1])
-        print(f"Created an alfred3 experiment template in the directory '{sys.argv[1]}'.")
+        dirname = secure_filename(sys.argv[1])
+        donwload_template(url, dirname)
+        print(f"Created an alfred3 experiment template in the directory '{dirname}'.")
 
     elif len(sys.argv) > 2:
         raise NotImplementedError("Currently, there are no arguments available for this module.")
 
     # print out success
-    print("You can start the experiment via 'python3 run.py'.")
+    print("You can start the experiment from within this directory via 'python3 run.py'.")
