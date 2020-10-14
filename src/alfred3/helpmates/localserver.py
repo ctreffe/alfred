1b0f7bc2cd32ebd33c8a5a72c61604d7466028ce
Johannes Brachem <jobrachem@posteo.de>
2020-10-14 11:32:21 +0200

    improve exception logging with localserver

--- src/alfred3/helpmates/localserver.py
+++ src/alfred3/helpmates/localserver.py
@@ -78,6 +78,7 @@ def start():
     session_id = script.config["exp_config"].get("metadata", "session_id")
     log = alfredlog.QueuedLoggingInterface("alfred3", f"exp.{exp_id}")
     log.session_id = session_id
+    script.log = log
 
     # generate experiment
     script.experiment = script.generate_experiment(config=script.config)
@@ -93,7 +94,11 @@ def start():
     log.info(log_msg)
 
     # start experiment
-    script.experiment.start()
+    try:
+        script.experiment.start()
+    except Exception:
+        log.exception("")
+        abort(500)
 
     # Experiment startup message
 
@@ -108,63 +113,66 @@ def start():
 
 @app.route("/experiment", methods=["GET", "POST"])
 def experiment():
+    try:
+        if request.method == "POST":
 
-    if request.method == "POST":
+            move = request.values.get("move", None)
+            directjump = request.values.get("directjump", None)
+            par = request.values.get("par", None)
+            page_token = request.values.get("page_token", None)
 
-        move = request.values.get("move", None)
-        directjump = request.values.get("directjump", None)
-        par = request.values.get("par", None)
-        page_token = request.values.get("page_token", None)
+            try:
+                token_list = session["page_tokens"]
+                token_list.remove(page_token)
+                session["page_tokens"] = token_list
+            except ValueError:
+                return redirect(url_for("experiment"))
 
-        try:
-            token_list = session["page_tokens"]
-            token_list.remove(page_token)
-            session["page_tokens"] = token_list
-        except ValueError:
+            kwargs = request.values.to_dict()
+            kwargs.pop("move", None)
+            kwargs.pop("directjump", None)
+            kwargs.pop("par", None)
+
+            script.experiment.user_interface_controller.update_with_user_input(kwargs)
+            if move is None and directjump is None and par is None and kwargs == {}:
+                pass
+            elif directjump and par:
+                posList = list(map(int, par.split(".")))
+                script.experiment.user_interface_controller.move_to_position(posList)
+            elif move == "started":
+                pass
+            elif move == "forward":
+                script.experiment.user_interface_controller.move_forward()
+            elif move == "backward":
+                script.experiment.user_interface_controller.move_backward()
+            elif move == "jump" and par and re.match(r"^\d+(\.\d+)*$", par):
+                posList = list(map(int, par.split(".")))
+                script.experiment.user_interface_controller.move_to_position(posList)
+            else:
+                abort(400)
             return redirect(url_for("experiment"))
 
-        kwargs = request.values.to_dict()
-        kwargs.pop("move", None)
-        kwargs.pop("directjump", None)
-        kwargs.pop("par", None)
+        elif request.method == "GET":
+            page_token = str(uuid4())
 
-        script.experiment.user_interface_controller.update_with_user_input(kwargs)
-        if move is None and directjump is None and par is None and kwargs == {}:
-            pass
-        elif directjump and par:
-            posList = list(map(int, par.split(".")))
-            script.experiment.user_interface_controller.move_to_position(posList)
-        elif move == "started":
-            pass
-        elif move == "forward":
-            script.experiment.user_interface_controller.move_forward()
-        elif move == "backward":
-            script.experiment.user_interface_controller.move_backward()
-        elif move == "jump" and par and re.match(r"^\d+(\.\d+)*$", par):
-            posList = list(map(int, par.split(".")))
-            script.experiment.user_interface_controller.move_to_position(posList)
-        else:
-            abort(400)
-        return redirect(url_for("experiment"))
+            # this block extracts the list "page_tokens", if it exists in the session
+            # it creates the list "page_tokens" as an empty list, if not. This is needed
+            # for qt-wk experiments because they don't call the route /start
+            try:
+                token_list = session["page_tokens"]
+            except KeyError:
+                token_list = []
 
-    elif request.method == "GET":
-        page_token = str(uuid4())
+            token_list.append(page_token)
+            session["page_tokens"] = token_list
 
-        # this block extracts the list "page_tokens", if it exists in the session
-        # it creates the list "page_tokens" as an empty list, if not. This is needed
-        # for qt-wk experiments because they don't call the route /start
-        try:
-            token_list = session["page_tokens"]
-        except KeyError:
-            token_list = []
-
-        token_list.append(page_token)
-        session["page_tokens"] = token_list
-
-        html = script.experiment.user_interface_controller.render_html(page_token)
-        resp = make_response(html)
-        resp.cache_control.no_cache = True
-        return resp
+            html = script.experiment.user_interface_controller.render_html(page_token)
+            resp = make_response(html)
+            resp.cache_control.no_cache = True
+            return resp
+    except Exception:
+        script.log.exception("")
+        abort(500)
 
 
 @app.route("/staticfile/<identifier>")
