197535c4e96ff24b29018bbd391a3672026c7dc9
Johannes Brachem <jobrachem@posteo.de>
2020-12-02 09:25:38 +0100

    deleted a line that lead to problems

--- src/alfred3/static/js/font-awesome-icons.js
+++ src/alfred3/static/js/font-awesome-icons.js
@@ -2219,7 +2219,6 @@
   function coerce(val) {
     // Getting an empty string will occur if the attribute is set on the HTML tag but without a value
     // We'll assume that this is an indication that it should be toggled to true
-    // For example <script data-search-pseudo-elements src="..."></script>
     if (val === '') return true;
     if (val === 'false') return false;
     if (val === 'true') return true;
