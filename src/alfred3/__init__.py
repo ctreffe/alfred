3e2207e7e360731ea290fca74b0f2abb8b9be620
Johannes Brachem <jobrachem@posteo.de>
2020-12-19 18:12:11 +0100

    update API import

--- src/alfred3/__init__.py
+++ src/alfred3/__init__.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
+from ._version import __version__
 
 from .experiment import Experiment
-from .page import *
+
+from .section import Section
+from .section import NoValidationSection
+from .section import CommitInputSection
+from .section import OnlyForwardSection
+
+from .page import Page
+from .page import WidePage
+from .page import UnlinkedDataPage
+from .page import CustomSavingPage
+
 from .element import *
-from .section import *
+
+from .util import emoji
+from .util import icon
+
+from .util import is_element
+from .util import is_input_element
+from .util import is_label
+from .util import is_page
+from .util import is_section
