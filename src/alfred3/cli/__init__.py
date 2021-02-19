548328db50903faa440d0e7d2c47d5b4c5a8d059
Johannes Brachem <jobrachem@posteo.de>
2021-02-19 13:55:15 +0100

    Update __init__.py

--- src/alfred3/cli/__init__.py
+++ src/alfred3/cli/__init__.py
@@ -1,3 +1,33 @@
+"""
+Command line interface for alfred3.
+
+Alfred3 offers some commands for the command line/terminal. You can see 
+a list of all available commands by executing the following in a terminal::
+
+    $ alfred3 --help
+
+Then, you can get further help on the specific commands. For example,
+to get help on the "run" command, execute::
+
+    $ alfred3 run --help
+
+These are the currently available commands::
+
+    $ alfred3 --help
+
+    Usage: alfred3 [OPTIONS] COMMAND [ARGS]...
+
+    Options:
+    --help  Show this message and exit.
+
+    Commands:
+    json-to-csv
+    run
+    template
+
+"""
+
+
 import click
 
 from .template import template
