36dee58b65e5dffdbc682bd8b756ee637bb071b9
Johannes Brachem <jobrachem@posteo.de>
2021-02-19 13:55:11 +0100

    Update template.py

--- src/alfred3/cli/template.py
+++ src/alfred3/cli/template.py
@@ -6,16 +6,18 @@ import click
 from alfred3 import files
 
 
-def _write(filename: str, path: Path = None):
+def _write(filename: str, out_filename: str = None, path: Path = None):
     fileobj = res.read_text(files, filename)
     
     if path is not None and path.is_dir():
         path.mkdir(parents=True, exist_ok=True)
     
+    out_filename = out_filename if out_filename is not None else filename
+
     if path is None:
-        filepath = Path.cwd() / filename
+        filepath = Path.cwd() / out_filename
     else:
-        filepath = path / filename
+        filepath = path / out_filename
     
     if filepath.exists() and filepath.is_file():
         click.echo(f"File '{filepath}' already exists. Skipping file.")
@@ -32,7 +34,7 @@ def _write(filename: str, path: Path = None):
 )
 def template(path: Path):
     _write(filename="script.py", path=path)
-    _write(filename="alfred.conf", path=path)
+    _write(filename="alfred.conf", out_filename="config.conf", path=path)
     click.echo("Template created. Start experiment with 'alfred3 run'.")
 
 
