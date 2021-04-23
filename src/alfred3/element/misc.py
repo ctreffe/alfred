fb81f785f0f14663cedbac9fa7c2622078388e5b
Johannes Brachem <jobrachem@posteo.de>
2021-04-23 21:28:53 +0200

    improve Callback and RepeatedCallback

--- src/alfred3/element/misc.py
+++ src/alfred3/element/misc.py
@@ -3,6 +3,7 @@ Provides elements that don't fit into the other categories.
 
 .. moduleauthor: Johannes Brachem <jbrachem@posteo.de>
 """
+import time
 
 from pathlib import Path
 from typing import Union
@@ -12,6 +13,7 @@ from jinja2 import Template
 from .core import Element
 from .core import InputElement
 from .core import jinja_env
+
 
 class Style(Element):
     """
@@ -58,7 +60,7 @@ class Style(Element):
                 def on_exp_access(self):
                     self += al.Text("Element 1", name="test_el")
                     self += al.Style(code="#test_el {color: red;}")
-        
+
         Minimal example, turning the color of all text elements on a
         specific page red. The elements are selected by their class::
 
@@ -71,10 +73,10 @@ class Style(Element):
 
                 def on_exp_access(self):
                     self += al.Style(code=".Text-element {color: red;}")
-                    
+
                     self += al.Text("Element 1", name="test_el1")
                     self += al.Text("Element 2", name="test_el2")
-                    
+
 
     """
 
@@ -92,13 +94,14 @@ class Style(Element):
         self.should_be_shown = False
 
         if (self.code and self.path) or (self.code and self.url) or (self.path and self.url):
-            raise ValueError("You can only specify one of 'code', 'url', or 'path'.")
+            raise ValueError(
+                "You can only specify one of 'code', 'url', or 'path'.")
 
     @property
     def css_code(self):
         if not self.code:
             return []
-        
+
         if self.path:
             p = self.experiment.subpath(self.path)
 
@@ -109,7 +112,7 @@ class Style(Element):
 
     @property
     def css_urls(self):
-        
+
         if self.url:
             return [(self.priority, self.url)]
         else:
@@ -133,7 +136,7 @@ class HideNavigation(Style):
 
         * By defining the :meth:`.Page.custom_move` method on a page,
           you can implement highly customized movement behavior.
-    
+
     Examples:
         Minimal example::
 
@@ -170,7 +173,7 @@ class JavaScript(Element):
             on a page. Lower numbers are included first. Can be useful, 
             if you have trouble with overriding code. In everyday use, 
             it's fine to stick with the default.
-    
+
     Notes:
         You can use the jquery API (version 3.5.1 as of 2021-01-20).
 
@@ -180,7 +183,7 @@ class JavaScript(Element):
           element CSS classes and IDs.
         * The method :meth:`.Element.add_js` can be used to add JS
           to a specific element.
-    
+
     Examples:
 
             In this example, we use JavaScript to create a text element
@@ -195,13 +198,13 @@ class JavaScript(Element):
                     name = "hello_world"
 
                     def on_exp_access(self):
-                        
+
                         js_code = '''
                         $( '#test_el' ).on('change', function() {
                             $( '#form' ).submit();
                         };)
                         '''
-                        
+
                         self += al.JavaScript(code=js_code)
                         self += al.Text("Element 1", name="test_el")
 
@@ -221,13 +224,14 @@ class JavaScript(Element):
         self.should_be_shown = False
 
         if (self.code and self.path) or (self.code and self.url) or (self.path and self.url):
-            raise ValueError("You can only specify one of 'code', 'url', or 'path'.")
+            raise ValueError(
+                "You can only specify one of 'code', 'url', or 'path'.")
 
     @property
     def js_code(self):
         if not self.code:
             return []
-        
+
         if self.path:
             p = self.experiment.subpath(self.path)
 
@@ -238,7 +242,7 @@ class JavaScript(Element):
 
     @property
     def js_urls(self):
-        
+
         if self.url:
             return [(self.priority, self.url)]
         else:
@@ -265,7 +269,7 @@ class WebExitEnabler(JavaScript):
 
                 def on_exp_access(self):
                     self += al.WebExitEnabler()
-              
+
 
     """
 
@@ -316,72 +320,205 @@ class Data(Value):
 
 
 class Callback(Element):
+    """
+    Triggers execution of a python function from a loaded page.
+
+    Args:
+        func (callable): Python function to be called on button click.
+            The function must take zero arguments, but it can be a 
+            method of a class or instance.
+
+        followup (str): What to do after the python function was called.
+            Can take the following values: 
+
+            - ``refresh`` submits and reloads the current page (default),
+            - ``none`` does nothing,
+            - ``forward`` submits the current page and moves forward,
+            - ``backward`` submits the current page and moves backward,
+            - ``jump>page_name`` submits the current page and triggers
+              a jump to a page with the name 'page_name'
+            - ``custom`` executes custom JavaScript. If you choose this 
+              option, you must supply your custom JavaScript through the
+              argument *custom_js*.
+
+        submit_first (bool): If True, the current values of all input
+            elements on the current page will be saved on button click,
+            before *func* is called. This way, these values will be
+            available in *func* through :attr:`.ExperimentSession.values`, 
+            if func has access to the ExperimentSession object.
+            See Example 3. Defaults to True.
+
+        delay (int): Number of seconds to wait before the callback is
+            triggered. Defaults to 0.
+
+        reset_delay (bool): If *True*, the delay will start from the 
+            beginning every time the page is reloaded, refreshed, or
+            revisited. Defaults to *False*.
+
+        custom_js (str): Custom JavaScript to execute after the python
+            function specified in *func* was called. Only takes effect,
+            if *followup* is set to 'custom'.
+
+    Examples:
+        
+        The callback on the first page will trigger after a delay of 10 
+        seconds. It will print the experiment id to the terminal::
+
+            import alfred3 as al
+            exp = al.Experiment()
+
+            @exp.member
+            class Demo(al.Page):
+
+                def on_exp_access(self):
+                    self += al.Callback(func=self.print_expid, delay=10)
+
+                def print_expid(self):
+                    print(self.exp.exp_id)
+    """
 
     web_widget = None
     should_be_shown = False
 
-    def __init__(self, func: callable, followup: str = "refresh"):
+    #: Javascript template
+    js_template = jinja_env.get_template("js/callback.js.j2")
+
+    def __init__(self, func: callable, followup: str = "refresh", submit_first: bool = True, delay: int = 0, reset_delay: bool = False, custom_js: str = ""):
         super().__init__()
         self.func = func
         self.followup = followup
         self.url = None
-    
+        self.delay_original = delay
+        self.delay = None
+        self.start_time = None
+        self.reset_delay = reset_delay
+        self.submit_first = submit_first
+        self.custom_js = custom_js
+
+    @property
+    def followup(self) -> str:
+        """
+        str: Value of the 'followup' setting for this element.
+        """
+        return self._followup
+
+    @followup.setter
+    def followup(self, value):
+        if value not in {"refresh", "forward", "backward", "none", "custom"} and not value.startswith("jump>"):
+            raise ValueError(f"Invalid value for 'followup': {value}")
+        self._followup = value
+
     def prepare_web_widget(self):
+        # docstring inherited
         self._js_code = []
         super().prepare_web_widget()
         self.url = self.exp.ui.add_callable(self.func)
-        
-        js_template = Template("""
-        $(document).ready(function () {
-    
-            $.get( "{{ url }}", function(data) {
-                
-                if ("{{ followup }}" == "refresh") {
-                    move(direction="stay");
-                } else if ("{{ followup }}" == "custom") {
-                    {{ custom_js }}
-                } else if ("{{ followup }}" != "none") {
-                    move(direction="{{ followup }}");
-                }
-            });
-            
-        });
-        """)
-        js = js_template.render(url=self.url, followup=self.followup)
+
+        if not self.start_time:
+            self.start_time = time.time()
+            self.delay = self.delay_original
+        elif not self.reset_delay:
+            now = time.time()
+            already_passed = now - self.start_time
+            self.delay = self.delay_original - already_passed
+
+        d = {}
+        d["url"] = self.url
+        d["followup"] = self.followup
+        d["timeout"] = self.delay
+        d["submit_first"] = self.submit_first
+        d["custom_js"] = self.custom_js
+
+        js = self.js_template.render(d)
+
         self.add_js(js)
 
 
 class RepeatedCallback(Element):
+    """
+    Triggers repeated execution of a python function from a loaded page.
+
+    Args:
+        func (callable): Python function to be called on button click.
+            The function must take zero arguments, but it can be a 
+            method of a class or instance.
+
+        interval (int): Number of seconds to wait between two calls to
+            *func*.
+
+        followup (str): What to do after the python function was called.
+            Can take the following values: 
+
+            - ``refresh`` submits and reloads the current page (default),
+            - ``none`` does nothing,
+            - ``forward`` submits the current page and moves forward,
+            - ``backward`` submits the current page and moves backward,
+            - ``jump>page_name`` submits the current page and triggers
+              a jump to a page with the name 'page_name'
+            - ``custom`` executes custom JavaScript. If you choose this 
+              option, you must supply your custom JavaScript through the
+              argument *custom_js*.
+
+        submit_first (bool): If True, the current values of all input
+            elements on the current page will be saved on button click,
+            before *func* is called. This way, these values will be
+            available in *func* through :attr:`.ExperimentSession.values`, 
+            if func has access to the ExperimentSession object.
+            See Example 3. Defaults to True.
+
+        reset_delay (bool): If *True*, the delay will start from the 
+            beginning every time the page is reloaded, refreshed, or
+            revisited. Defaults to *False*.
+
+        custom_js (str): Custom JavaScript to execute after the python
+            function specified in *func* was called. Only takes effect,
+            if *followup* is set to 'custom'.
+    
+    Examples:
+
+        The callback on the first page will trigger every 10 seconds.
+        It will print the experiment id to the terminal::
+
+            import alfred3 as al
+            exp = al.Experiment()
+
+            @exp.member
+            class Demo(al.Page):
+
+                def on_exp_access(self):
+                    self += al.RepeatedCallback(func=self.print_expid, interval=10)
+
+                def print_expid(self):
+                    print(self.exp.exp_id)
+    """
 
     web_widget = None
     should_be_shown = False
 
-    def __init__(self, func: callable, submit_first: bool = True, interval: int = 1):
+    #: Javascript template
+    js_template = jinja_env.get_template("js/repeatedcallback.js.j2")
+
+    def __init__(self, func: callable, interval: int, followup: str = "none", submit_first: bool = True, custom_js: str = ""):
         super().__init__()
         self.func = func
         self.interval = interval
         self.submit_first = submit_first
         self.url = None
-    
+        self.followup = followup
+        self.custom_js = custom_js
+
     def prepare_web_widget(self):
+        # docstring inherited
         self._js_code = []
         super().prepare_web_widget()
         self.url = self.exp.ui.add_callable(self.func)
-        
-        js_template = Template("""
-        var ping = function() {
-            
-            {% if submit_first %}
-                var data = $("#form").serializeArray();
-                $.post("/callable/set_page_data", data);
-            {% endif %}
 
-            $.get( "{{ url }}" );
-        };
+        d = {}
+        d["url"] = self.url
+        d["followup"] = self.followup
+        d["interval"] = self.interval
+        d["submit_first"] = self.submit_first
+        d["custom_js"] = self.custom_js
 
-        $(document).ready(function () {
-            setInterval(ping, {{ interval }} * 1000)
-        });
-        """)
-        js = js_template.render(url=self.url, submit_first=self.submit_first, interval=self.interval)
+        js = self.js_template.render(d)
         self.add_js(js)
