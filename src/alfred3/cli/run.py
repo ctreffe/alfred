770de798f7fc84c3847b9e8af926855dc125c775
Johannes Brachem <jobrachem@posteo.de>
2021-10-21 22:44:57 +0200

    enable starting test mode from CLI

--- src/alfred3/cli/run.py
+++ src/alfred3/cli/run.py
@@ -14,8 +14,14 @@ from alfred3.run import ExperimentRunner
     "-debug/-production",
     "--debug/--production",
     default=False,
-    help="If this flag is set to to '-debug', the alfred experiment will start in flask's debug mode. [default: '-production']",
+    help="If this flag is set to to '-debug', the alfred experiment will start in debug mode. [default: '-production']",
 )
-def run(path, auto_open, debug):
+@click.option(
+    "-test/-production",
+    "--test/--production",
+    default=False,
+    help="If this flag is set to to '-test', the alfred experiment will start in test mode. [default: '-production']",
+)
+def run(path, auto_open, debug, test):
     runner = ExperimentRunner(path)
-    runner.auto_run(open_browser=auto_open, debug=debug)
+    runner.auto_run(open_browser=auto_open, debug=debug, test=test)
