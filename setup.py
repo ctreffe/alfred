0b5abdb9c9a7be504a03f18c463174db9d7841cf
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2022-05-23 20:51:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setuptools.setup(
         "bleach>=3.3",
     ],
     extras_require={
-        "dev": ["pytest", "pre-commit", "black", "flake8", "python-dotenv", "bs4"]
+        "dev": ["pytest", "pre-commit", "black", "flake8", "python-dotenv", "bs4", "selenium"]
     },
     entry_points="""
     [console_scripts]
