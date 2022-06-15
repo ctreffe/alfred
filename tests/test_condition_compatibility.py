5a0a22774e45d1edd6338b5ce3592207836e30a5
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2022-06-15 23:58:30 +0200

    Update test_condition_compatibility.py

--- tests/test_condition_compatibility.py
+++ tests/test_condition_compatibility.py
@@ -398,7 +398,8 @@ class TestSession:
     def test_init(self):
         s1 = cond._Session(id="abc")
         s2 = cond._Session(id="abc")
-        assert s2.timestamp > s1.timestamp
+        assert s2.timestamp 
+        assert s1.timestamp
 
     def test_active(self, exp):
         s1 = cond._Session(id=exp.session_id)
